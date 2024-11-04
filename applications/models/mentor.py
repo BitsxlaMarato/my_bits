3fe13c892bf87f27a60e359f49a94e8682472c8c
Gerard Madrid <gerardm27@gmail.com>
2024-11-04 22:19:22 +0100

    Auto-invite mentors, to skip everything else

--- applications/models/mentor.py
+++ applications/models/mentor.py
@@ -7,6 +7,7 @@ class MentorApplication(
 
     valid = models.BooleanField(default=False)
     type_of_helper = models.CharField(max_length=100, null=True, blank=True, choices=TYPE_OF_HELPER_CHOICES)
+    status = models.CharField(max_length=50, default=APP_CONFIRMED)  
     
     def can_be_edit(self, app_type="M"):
         return self.status in [APP_PENDING, APP_DUBIOUS] and not utils.is_app_closed(app_type)
